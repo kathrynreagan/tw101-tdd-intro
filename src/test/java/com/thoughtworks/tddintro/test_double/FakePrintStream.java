9aa1f4845e3451f5bd0ef4e320c17b3655af7fa3
Kathryn Reagan <kathryn.m.reagan@gmail.com>
2016-06-02 22:06:49 -0500

    added comments explaing stubs

--- src/test/java/com/thoughtworks/tddintro/test_double/FakePrintStream.java
+++ src/test/java/com/thoughtworks/tddintro/test_double/FakePrintStream.java
@@ -1,3 +1,9 @@
+/* this class is a STUB!!!!
+ * stubs are: modules of code that simulate the behaviors of
+ * software components that a module under test depends on
+ * (the module under test is GreetingPrinter)
+ */
+
 package com.thoughtworks.tddintro.test_double;
 
 import java.io.PrintStream;
